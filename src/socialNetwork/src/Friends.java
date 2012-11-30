c2b090d59c39263e1f7647a03c0f1dc8f58ad98a
Matthieu Desplat <mdesplat@emi.u-bordeaux1.fr>
2012-11-30 17:36:34 +0100

    Change xml protocol.

--- src/socialNetwork/src/Friends.java
+++ src/socialNetwork/src/Friends.java
@@ -1,23 +1,33 @@
 package socialNetwork.src;
 
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+//import javax.xml.parsers.DocumentBuilder;
+//import javax.xml.parsers.DocumentBuilderFactory;
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Element;
+//import org.w3c.dom.Node;
+//import org.w3c.dom.NodeList;
+import java.io.*;
+import org.jdom2.*;
+import org.jdom2.output.*;
+import org.jdom2.input.*;
+import org.jdom2.filter.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 public class Friends{
 
 	private String name;
 	private String host;
 	private boolean status;
+	
+	static Element racine = new Element("personnes");
+	static org.jdom2.Document document = new Document(racine);
 
 	public Friends(String name, String host, String status){
 		this.name = name;
 		this.host = host;
-		System.out.println(status);
 		if(status.compareTo("false") == 0)
 			this.status = false;
 		else
@@ -49,31 +59,54 @@ public class Friends{
 		this.status = false;
 		//change dans xml
 	}
-
-	/*Récupération liste d'amis*/
 	
-	public static Friends[] getFriends() throws Exception{
-		Friends[] listFriend;
-		File list = new File("friendList.xml");
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-		Document doc = dBuilder.parse(list);
-		doc.getDocumentElement().normalize();
-		NodeList nodes = doc.getElementsByTagName("friend");
-		listFriend = new Friends[nodes.getLength()];
-		for (int i = 0; i < nodes.getLength(); i++) {
-			Node node = nodes.item(i);
-			if (node.getNodeType() == Node.ELEMENT_NODE) {
-				Element element = (Element) node;
-				listFriend[i] = new Friends(getValue("name", element), getValue("host", element), getValue("friendStatus", element));
-			}
+	static void lireFichierXML(String fichier) throws Exception{
+		SAXBuilder sxb = new SAXBuilder(); 
+		document = sxb.build(new File(fichier));
+		racine = document.getRootElement();
+	}
+	
+	/*Récupération liste d'amis*/
+	public static ArrayList<Friends> getFriends(){
+		ArrayList<Friends> listFriends = new ArrayList<>();
+		try {
+			lireFichierXML("friendList");
+		racine.getChildren();
+		List listTmp = racine.getChildren("friend");
+		Iterator i = listTmp.iterator();
+		while(i.hasNext()){
+			Element courant = (Element)i.next();
+			listFriends.add(new Friends(courant.getChild("name").getText(), courant.getChild("host").getText(), courant.getChild("friendStatus").toString()));
 		}
-		return listFriend;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listFriends;
 	}
 
-	private static String getValue(String tag, Element element) {
-		NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
-		Node node = (Node) nodes.item(0);
-		return node.getNodeValue();
-	}
+//	public static Friends[] getFriends() throws Exception{
+//		Friends[] listFriend;
+//		File list = new File("friendList.xml");
+//		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+//		Document doc = dBuilder.parse(list);
+//		doc.getDocumentElement().normalize();
+//		NodeList nodes = doc.getElementsByTagName("friend");
+//		listFriend = new Friends[nodes.getLength()];
+//		for (int i = 0; i < nodes.getLength(); i++) {
+//			Node node = nodes.item(i);
+//			if (node.getNodeType() == Node.ELEMENT_NODE) {
+//				Element element = (Element) node;
+//				listFriend[i] = new Friends(getValue("name", element), getValue("host", element), getValue("friendStatus", element));
+//			}
+//		}
+//		return listFriend;
+//	}
+//
+//	private static String getValue(String tag, Element element) {
+//		NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
+//		Node node = (Node) nodes.item(0);
+//		return node.getNodeValue();
+//	}
 }
