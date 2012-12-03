9ea6c062cd3128253876a05084967bcf2b8927d0
Mehyo <zibo09@gmail.com>
2012-12-03 17:34:52 +0100

    Bug correction.

--- src/socialNetwork/src/XmlTreatment.java
+++ src/socialNetwork/src/XmlTreatment.java
@@ -12,24 +12,24 @@ import java.util.List;
 import java.util.Iterator;
 
 public class XmlTreatment {
-	
+
 	static Element racine = new Element("personnes");
 	static org.jdom2.Document document = new Document(racine);
 
 	static void readFileXML(String fichier) throws Exception{
-		SAXBuilder sxb = new SAXBuilder(); 
+		SAXBuilder sxb = new SAXBuilder();
 		document = sxb.build(new File(fichier));
 		racine = document.getRootElement();
 	}
-	
-//	static void enregistreFichier(String fichier){
-//		try{
-//	         XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-//	         sortie.output(document, new FileOutputStream(fichier));
-//	   }
-//	catch(java.io.IOException e){}
-//	}
-//	
+
+	// static void enregistreFichier(String fichier){
+	// try{
+	// XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+	// sortie.output(document, new FileOutputStream(fichier));
+	// }
+	// catch(java.io.IOException e){}
+	// }
+	//
 	public static void deleteFriendXML(String name){
 		List listElement = racine.getChildren("name");
 		Iterator i = listElement.iterator();
@@ -41,7 +41,7 @@ public class XmlTreatment {
 			}
 		}
 	}
-	
+
 	public static void addFriendXML(String friendName, String friendHost, String status) throws Exception{
 		readFileXML("friendList");
 		Element friend = new Element("friend");
@@ -55,23 +55,22 @@ public class XmlTreatment {
 		Attribute friendPicture = new Attribute("Picture", "none");
 		friend.setAttribute(friendStatus);
 	}
-	
+
 	/*Récupération liste d'amis*/
 	public static ArrayList<Friends> getFriendsXML(){
-		ArrayList<Friends> listFriends = new ArrayList<>();
+		ArrayList<Friends> listFriends = new ArrayList<Friends>();
 		try {
 			readFileXML("friendList");
-		racine.getChildren();
-		List listTmp = racine.getChildren("friend");
-		Iterator i = listTmp.iterator();
-		while(i.hasNext()){
-			Element courant = (Element)i.next();
-			listFriends.add(new Friends(courant.getChild("name").getText(), courant.getChild("host").getText(), courant.getChild("friendStatus").toString()));
-		}
+			racine.getChildren();
+			List listTmp = racine.getChildren("friend");
+			Iterator i = listTmp.iterator();
+			while(i.hasNext()){
+				Element courant = (Element)i.next();
+				listFriends.add(new Friends(courant.getChild("name").getText(), courant.getChild("host").getText(), courant.getChild("friendStatus").toString()));
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return listFriends;
 	}
-}
